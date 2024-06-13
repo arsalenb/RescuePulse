4dd166f3e563b068f7a29a4e83fd4a969b19ce41
arsalenb <arselenebidani@yahoo.fr>
2024-06-14 00:23:51 +0200

    Project boilerplate

---
+++ mobile/src/main/java/it/unipi/RescuePulse/mobile/MobileMainActivity.kt
@@ -0,0 +1,47 @@
+package it.unipi.RescuePulse.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import it.unipi.RescuePulse.mobile.ui.theme.RescuePulseTheme
+
+class MobileMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RescuePulseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RescuePulseTheme {
+        Greeting("Android")
+    }
+}
