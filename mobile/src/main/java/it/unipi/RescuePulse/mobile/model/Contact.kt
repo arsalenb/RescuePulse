be4caf16d087189f3fadc547d14a33133faa841f
arsalenb <arselenebidani@yahoo.fr>
2024-06-22 20:46:08 +0200

    Added serialization and deserialization methods to Contact data model

--- mobile/src/main/java/it/unipi/RescuePulse/mobile/model/Contact.kt
+++ mobile/src/main/java/it/unipi/RescuePulse/mobile/model/Contact.kt
@@ -3,16 +3,28 @@ package it.unipi.RescuePulse.mobile.model
 data class Contact(val displayName: String, val phoneNumber: String?) {
     // Companion object for handling serialization and deserialization
     companion object {
-        fun fromString(string: String): Contact {
+        private fun fromString(string: String): Contact {
             val parts = string.split(",")
             val displayName = parts[0]
             val phoneNumber = if (parts.size > 1) parts[1] else null
             return Contact(displayName, phoneNumber)
         }
-    }
 
-    // Override toString() to convert Contact to string representation
-    override fun toString(): String {
-        return "$displayName, $phoneNumber"
+        // Deserialize a JSON string into a list of Contact objects
+        fun fromJsonList(json: String?): MutableList<Contact> {
+            val contactList = mutableListOf<Contact>()
+            json?.let {
+                val parts = it.split(";")
+                for (part in parts) {
+                    contactList.add(fromString(part))
+                }
+            }
+            return contactList
+        }
+
+        // Serialize a list of Contact objects into a JSON string
+        fun toJsonList(contacts: MutableList<Contact>): String {
+            return contacts.joinToString(";") { it.toString() }
+        }
     }
 }
